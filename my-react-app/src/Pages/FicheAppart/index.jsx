7820fba5b6cc3abff679fe8582ad1d2c67f0a4fd
chafaa life <chafaa2024@outlook.com>
2025-03-18 23:08:52 +0100

    FicheAppart page: slider operational + Collapse component: accordions op with smooth transitions

--- my-react-app/src/Pages/FicheAppart/index.jsx
+++ my-react-app/src/Pages/FicheAppart/index.jsx
@@ -2,17 +2,24 @@ import AppartList from '../../data/AppartList.json'
 import { Link, useParams } from 'react-router-dom'
 import { useState, useEffect } from 'react'
 import '../../styles/FicheAppart.scss'
-import { chevronUp } from '../../components/CollapseMenu'
+import { FaRegStar  } from "react-icons/fa";
+
+// import { chevronUp } from '../../components/CollapseMenu'
+const arrowBtn = <svg xmlns="http://www.w3.org/2000/svg" height="48px" viewBox="0 -960 960 960" width="48px" fill="#FFFFFF"><path d="M304-68.43 234.43-139l343-343-343-343L304-895.57 717.57-482 304-68.43Z"/></svg>
 function FicheAppart(){
     let params = useParams()
     const filtered = AppartList.filter(appart=>appart.id===params.id)
     const pictures = filtered.map(pic=>pic.pictures)
+    const tags = filtered.map(item=>item.tags)
     
 const [slideIndex, setSlideIndex] = useState(0)
-const [arrowButton, setArrowButton] = useState(true)    
+const [arrowButton, setArrowButton] = useState(true) 
+  
    function nextSlide(index){
-    setArrowButton(true)
+        setArrowButton(true)
+        
         setSlideIndex(s=>s+index)
+        
         if(slideIndex >= pictures[0].length-1){
             
             setSlideIndex(0)
@@ -25,19 +32,41 @@ const [arrowButton, setArrowButton] = useState(true)
         setSlideIndex(s=>s - index)
         if(slideIndex==0){setSlideIndex(pictures[0].length - 1)}
     }
+    function checkSlide(){
+        if(pictures[0].length<=0){setArrowButton(false)}
+    }
+    document.addEventListener('DOMContentLoaded',checkSlide)
     return(
         
-        <div className="caroussel">
-            <h1>Fiche Logement</h1>
+        <main className="house-content">
+            
             <div className="slider">
-            {arrowButton && (<button className='next-button' onClick={()=>nextSlide(1)}>{chevronUp}</button>)}
-            <button className='prev-button' onClick={()=>prevSlide(1)}>{chevronUp}</button>
-             {/* {filtered.map(appart=><img src={appart.pictures[slideIndex]} style={{height:'500px', width:'100%'}}/>)} */}
-             <img className='slider-item' src={pictures[0][slideIndex]} alt="picture" />
-             <span className='slider-pagination'>{slideIndex+1}/{pictures[0].length}</span>
+                
+                {arrowButton?<button className='next-button' onClick={()=>nextSlide(1)}>{arrowBtn}</button>:null}
+                <button className='prev-button' onClick={()=>prevSlide(1)}>{arrowBtn}</button>
+                {/* {filtered.map(appart=><img src={appart.pictures[slideIndex]} style={{height:'500px', width:'100%'}}/>)} */}
+                <img className='slider-item' src={pictures[0][slideIndex]} alt="picture" />
+                <span className='slider-pagination'>{slideIndex+1}/{pictures[0].length}</span>
                 
             </div>
-        </div>
+        <section className="house-properties">
+            <div className="title-and-host">
+                <h2 className='title'>{filtered.map(item=>item.title)}</h2>
+                <h4 className='host'>{filtered.map(item=>item.host.name)}</h4>
+                <div className='ball' style={{height:64, width:64, backgroundColor:'#C4C4C4', borderRadius:'50%'}}></div>
+            </div>
+            <div className='location'>
+                <h3 className="house-location">{filtered.map(item=>item.location)}</h3>
+            </div>
+            <div className="tags-rating">
+               
+               <ul>
+                {tags[0].map((item, index)=><li key={index} className='tag'>{item}</li>)}
+                </ul> 
+              {filtered.map(item=><span>{item.rating}{[...map(5,i)=<FaRegStar color='red'/>]}</span>)}
+            </div>
+        </section>
+        </main>
     )
 }
 export default FicheAppart
--- my-react-app/src/components/CollapseMenu/index.jsx
+++ my-react-app/src/components/CollapseMenu/index.jsx
@@ -1,12 +1,21 @@
 import '../../styles/Collapse.scss'
-import { useState, useEffect } from 'react'
+import { useState, useEffect,usere, useRef } from 'react'
 export const chevronUp = <svg xmlns="http://www.w3.org/2000/svg" height="40px" viewBox="0 -960 960 960" 
                         width="40px" fill="#fff">
                         <path d="M480-528 296-344l-56-56 240-240 240 240-56 56-184-184Z"/>
                     </svg>
 
 function CollapseMenu({property, description}){
-    const [isActive, setIsActive] = useState(false)
+    const [isActive, setIsActive] = useState(true)
+    const ContentRef = useRef(null)
+    const [height, setHeight] = useState(0)
+    useEffect(()=>{
+        if(isActive){
+        setHeight(ContentRef.current.scrollHeight)
+    }else{
+        setHeight(0)
+    }
+    }, [isActive])
 function toggleText(){
     setIsActive(isActive=>!isActive)
 }
@@ -19,7 +28,8 @@ function toggleText(){
                 <button className={`arrow ${isActive? 'active' : ''}`} onClick={toggleText}>{chevronUp}</button>
                 
                 </div>
-                <p className={`description ${isActive ? 'display': ""}`}>{description}</p>
+                {/* <p className={`description ${isActive ? 'display': ""}`}>{description}</p> */}
+                <p ref={ContentRef} className='description' style={{height:`${height}px`}}>{description}</p>
             </article>
            
     )
